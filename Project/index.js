32bb45f0d750a0785742dcdf0d34ec9f68cfdfd9
rubenyw <rubenyasonwinarta@gmail.com>
2023-04-12 02:53:59 +0700

    Update index.js

--- Project/index.js
+++ Project/index.js
@@ -4,8 +4,12 @@ const db = require('./src/databases/database');
 const app = express();
 const port = 3000;
 
+const temp = require('./src/routes/temp');
+
 db.authenticate();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use('/api/temp', temp);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
