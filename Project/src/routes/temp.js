08a34583cc098f54279b449c27d39d890a53e64c
rubenyw <rubenyasonwinarta@gmail.com>
2023-05-15 11:45:15 +0700

    Delete temp.js

--- Project/src/routes/temp.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { tempQuery } = require("../controllers/temp");
-
-const router = express.Router();
-
-router.get("/", tempQuery);
-
-module.exports = router;
