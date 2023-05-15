80dfb00341dd3eb4d6ebbff9bd32ea09d99f911c
steveharianto <steveharianto@gmail.com>
2023-05-15 11:38:56 +0700

    Update temp.js

--- Project/src/routes/temp.js
+++ Project/src/routes/temp.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const {tempQuery} = require('../controllers/temp');
+const express = require("express");
+const { tempQuery } = require("../controllers/temp");
 
 const router = express.Router();
 
-router.get('/', tempQuery);
+router.get("/", tempQuery);
 
 module.exports = router;
