4d37e2d87f1132817bf796201ce9746ac5fbf376
rubenyw <rubenyasonwinarta@gmail.com>
2023-05-17 23:28:57 +0700

    Update temp.js

--- Project/src/controllers/temp.js
+++ Project/src/controllers/temp.js
@@ -5,11 +5,8 @@ const Joi = require("joi");
 const Temp = require("../models/temp");
 
 const tempQuery = async (req, res) => {
-    const result = await axios.get("https://api.rajaongkir.com/starter/city", {
-        id: 39,
-        province: 5,
-    });
-    console.log(result);
+    const result = await axios.get("https://api.rajaongkir.com/starter/city");
+
     return res.status(201).json({
         msg: "Ha",
     });
