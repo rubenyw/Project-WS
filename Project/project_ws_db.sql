8ebdd55c3d4e8aa78b51cc52f6ee5b6ca6b83782
rubenyw <rubenyasonwinarta@gmail.com>
2023-05-17 23:47:26 +0700

    update db dan bug

--- Project/project_ws_db.sql
+++ Project/project_ws_db.sql
@@ -1,36 +1,27 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 15, 2023 at 08:04 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+/*
+SQLyog Community v13.1.9 (64 bit)
+MySQL - 10.4.24-MariaDB : Database - project_ws
+*********************************************************************
+*/
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+/*!40101 SET NAMES utf8 */;
 
+/*!40101 SET SQL_MODE=''*/;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`project_ws` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
---
--- Database: `project_ws`
---
+USE `project_ws`;
 
--- --------------------------------------------------------
+/*Table structure for table `barang` */
 
-create database project_ws;
-use project_ws;
---
--- Table structure for table `barang`
---
+DROP TABLE IF EXISTS `barang`;
 
 CREATE TABLE `barang` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_sender` int(11) NOT NULL,
   `id_traveller` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
@@ -39,54 +30,575 @@ CREATE TABLE `barang` (
   `id_kota_tujuan` int(11) NOT NULL,
   `durasi` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
-  `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `status` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_sender` (`id_sender`),
+  KEY `id_traveller` (`id_traveller`),
+  KEY `id_kota_keberangkatan` (`id_kota_keberangkatan`),
+  KEY `id_kota_tujuan` (`id_kota_tujuan`),
+  CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_sender`) REFERENCES `user` (`id`),
+  CONSTRAINT `barang_ibfk_2` FOREIGN KEY (`id_traveller`) REFERENCES `user` (`id`),
+  CONSTRAINT `barang_ibfk_3` FOREIGN KEY (`id_kota_keberangkatan`) REFERENCES `kota` (`id`),
+  CONSTRAINT `barang_ibfk_4` FOREIGN KEY (`id_kota_tujuan`) REFERENCES `kota` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+/*Data for the table `barang` */
 
---
--- Table structure for table `kota`
---
+/*Table structure for table `kota` */
+
+DROP TABLE IF EXISTS `kota`;
 
 CREATE TABLE `kota` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=502 DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+/*Data for the table `kota` */
 
---
--- Table structure for table `ktp`
---
+insert  into `kota`(`id`,`nama`) values 
+(1,'Aceh Barat'),
+(2,'Aceh Barat Daya'),
+(3,'Aceh Besar'),
+(4,'Aceh Jaya'),
+(5,'Aceh Selatan'),
+(6,'Aceh Singkil'),
+(7,'Aceh Tamiang'),
+(8,'Aceh Tengah'),
+(9,'Aceh Tenggara'),
+(10,'Aceh Timur'),
+(11,'Aceh Utara'),
+(12,'Agam'),
+(13,'Alor'),
+(14,'Ambon'),
+(15,'Asahan'),
+(16,'Asmat'),
+(17,'Badung'),
+(18,'Balangan'),
+(19,'Balikpapan'),
+(20,'Banda Aceh'),
+(21,'Bandar Lampung'),
+(22,'Bandung'),
+(23,'Bandung'),
+(24,'Bandung Barat'),
+(25,'Banggai'),
+(26,'Banggai Kepulauan'),
+(27,'Bangka'),
+(28,'Bangka Barat'),
+(29,'Bangka Selatan'),
+(30,'Bangka Tengah'),
+(31,'Bangkalan'),
+(32,'Bangli'),
+(33,'Banjar'),
+(34,'Banjar'),
+(35,'Banjarbaru'),
+(36,'Banjarmasin'),
+(37,'Banjarnegara'),
+(38,'Bantaeng'),
+(39,'Bantul'),
+(40,'Banyuasin'),
+(41,'Banyumas'),
+(42,'Banyuwangi'),
+(43,'Barito Kuala'),
+(44,'Barito Selatan'),
+(45,'Barito Timur'),
+(46,'Barito Utara'),
+(47,'Barru'),
+(48,'Batam'),
+(49,'Batang'),
+(50,'Batang Hari'),
+(51,'Batu'),
+(52,'Batu Bara'),
+(53,'Bau-Bau'),
+(54,'Bekasi'),
+(55,'Bekasi'),
+(56,'Belitung'),
+(57,'Belitung Timur'),
+(58,'Belu'),
+(59,'Bener Meriah'),
+(60,'Bengkalis'),
+(61,'Bengkayang'),
+(62,'Bengkulu'),
+(63,'Bengkulu Selatan'),
+(64,'Bengkulu Tengah'),
+(65,'Bengkulu Utara'),
+(66,'Berau'),
+(67,'Biak Numfor'),
+(68,'Bima'),
+(69,'Bima'),
+(70,'Binjai'),
+(71,'Bintan'),
+(72,'Bireuen'),
+(73,'Bitung'),
+(74,'Blitar'),
+(75,'Blitar'),
+(76,'Blora'),
+(77,'Boalemo'),
+(78,'Bogor'),
+(79,'Bogor'),
+(80,'Bojonegoro'),
+(81,'Bolaang Mongondow (Bolmong)'),
+(82,'Bolaang Mongondow Selatan'),
+(83,'Bolaang Mongondow Timur'),
+(84,'Bolaang Mongondow Utara'),
+(85,'Bombana'),
+(86,'Bondowoso'),
+(87,'Bone'),
+(88,'Bone Bolango'),
+(89,'Bontang'),
+(90,'Boven Digoel'),
+(91,'Boyolali'),
+(92,'Brebes'),
+(93,'Bukittinggi'),
+(94,'Buleleng'),
+(95,'Bulukumba'),
+(96,'Bulungan (Bulongan)'),
+(97,'Bungo'),
+(98,'Buol'),
+(99,'Buru'),
+(100,'Buru Selatan'),
+(101,'Buton'),
+(102,'Buton Utara'),
+(103,'Ciamis'),
+(104,'Cianjur'),
+(105,'Cilacap'),
+(106,'Cilegon'),
+(107,'Cimahi'),
+(108,'Cirebon'),
+(109,'Cirebon'),
+(110,'Dairi'),
+(111,'Deiyai (Deliyai)'),
+(112,'Deli Serdang'),
+(113,'Demak'),
+(114,'Denpasar'),
+(115,'Depok'),
+(116,'Dharmasraya'),
+(117,'Dogiyai'),
+(118,'Dompu'),
+(119,'Donggala'),
+(120,'Dumai'),
+(121,'Empat Lawang'),
+(122,'Ende'),
+(123,'Enrekang'),
+(124,'Fakfak'),
+(125,'Flores Timur'),
+(126,'Garut'),
+(127,'Gayo Lues'),
+(128,'Gianyar'),
+(129,'Gorontalo'),
+(130,'Gorontalo'),
+(131,'Gorontalo Utara'),
+(132,'Gowa'),
+(133,'Gresik'),
+(134,'Grobogan'),
+(135,'Gunung Kidul'),
+(136,'Gunung Mas'),
+(137,'Gunungsitoli'),
+(138,'Halmahera Barat'),
+(139,'Halmahera Selatan'),
+(140,'Halmahera Tengah'),
+(141,'Halmahera Timur'),
+(142,'Halmahera Utara'),
+(143,'Hulu Sungai Selatan'),
+(144,'Hulu Sungai Tengah'),
+(145,'Hulu Sungai Utara'),
+(146,'Humbang Hasundutan'),
+(147,'Indragiri Hilir'),
+(148,'Indragiri Hulu'),
+(149,'Indramayu'),
+(150,'Intan Jaya'),
+(151,'Jakarta Barat'),
+(152,'Jakarta Pusat'),
+(153,'Jakarta Selatan'),
+(154,'Jakarta Timur'),
+(155,'Jakarta Utara'),
+(156,'Jambi'),
+(157,'Jayapura'),
+(158,'Jayapura'),
+(159,'Jayawijaya'),
+(160,'Jember'),
+(161,'Jembrana'),
+(162,'Jeneponto'),
+(163,'Jepara'),
+(164,'Jombang'),
+(165,'Kaimana'),
+(166,'Kampar'),
+(167,'Kapuas'),
+(168,'Kapuas Hulu'),
+(169,'Karanganyar'),
+(170,'Karangasem'),
+(171,'Karawang'),
+(172,'Karimun'),
+(173,'Karo'),
+(174,'Katingan'),
+(175,'Kaur'),
+(176,'Kayong Utara'),
+(177,'Kebumen'),
+(178,'Kediri'),
+(179,'Kediri'),
+(180,'Keerom'),
+(181,'Kendal'),
+(182,'Kendari'),
+(183,'Kepahiang'),
+(184,'Kepulauan Anambas'),
+(185,'Kepulauan Aru'),
+(186,'Kepulauan Mentawai'),
+(187,'Kepulauan Meranti'),
+(188,'Kepulauan Sangihe'),
+(189,'Kepulauan Seribu'),
+(190,'Kepulauan Siau Tagulandang Biaro (Sitaro)'),
+(191,'Kepulauan Sula'),
+(192,'Kepulauan Talaud'),
+(193,'Kepulauan Yapen (Yapen Waropen)'),
+(194,'Kerinci'),
+(195,'Ketapang'),
+(196,'Klaten'),
+(197,'Klungkung'),
+(198,'Kolaka'),
+(199,'Kolaka Utara'),
+(200,'Konawe'),
+(201,'Konawe Selatan'),
+(202,'Konawe Utara'),
+(203,'Kotabaru'),
+(204,'Kotamobagu'),
+(205,'Kotawaringin Barat'),
+(206,'Kotawaringin Timur'),
+(207,'Kuantan Singingi'),
+(208,'Kubu Raya'),
+(209,'Kudus'),
+(210,'Kulon Progo'),
+(211,'Kuningan'),
+(212,'Kupang'),
+(213,'Kupang'),
+(214,'Kutai Barat'),
+(215,'Kutai Kartanegara'),
+(216,'Kutai Timur'),
+(217,'Labuhan Batu'),
+(218,'Labuhan Batu Selatan'),
+(219,'Labuhan Batu Utara'),
+(220,'Lahat'),
+(221,'Lamandau'),
+(222,'Lamongan'),
+(223,'Lampung Barat'),
+(224,'Lampung Selatan'),
+(225,'Lampung Tengah'),
+(226,'Lampung Timur'),
+(227,'Lampung Utara'),
+(228,'Landak'),
+(229,'Langkat'),
+(230,'Langsa'),
+(231,'Lanny Jaya'),
+(232,'Lebak'),
+(233,'Lebong'),
+(234,'Lembata'),
+(235,'Lhokseumawe'),
+(236,'Lima Puluh Koto/Kota'),
+(237,'Lingga'),
+(238,'Lombok Barat'),
+(239,'Lombok Tengah'),
+(240,'Lombok Timur'),
+(241,'Lombok Utara'),
+(242,'Lubuk Linggau'),
+(243,'Lumajang'),
+(244,'Luwu'),
+(245,'Luwu Timur'),
+(246,'Luwu Utara'),
+(247,'Madiun'),
+(248,'Madiun'),
+(249,'Magelang'),
+(250,'Magelang'),
+(251,'Magetan'),
+(252,'Majalengka'),
+(253,'Majene'),
+(254,'Makassar'),
+(255,'Malang'),
+(256,'Malang'),
+(257,'Malinau'),
+(258,'Maluku Barat Daya'),
+(259,'Maluku Tengah'),
+(260,'Maluku Tenggara'),
+(261,'Maluku Tenggara Barat'),
+(262,'Mamasa'),
+(263,'Mamberamo Raya'),
+(264,'Mamberamo Tengah'),
+(265,'Mamuju'),
+(266,'Mamuju Utara'),
+(267,'Manado'),
+(268,'Mandailing Natal'),
+(269,'Manggarai'),
+(270,'Manggarai Barat'),
+(271,'Manggarai Timur'),
+(272,'Manokwari'),
+(273,'Manokwari Selatan'),
+(274,'Mappi'),
+(275,'Maros'),
+(276,'Mataram'),
+(277,'Maybrat'),
+(278,'Medan'),
+(279,'Melawi'),
+(280,'Merangin'),
+(281,'Merauke'),
+(282,'Mesuji'),
+(283,'Metro'),
+(284,'Mimika'),
+(285,'Minahasa'),
+(286,'Minahasa Selatan'),
+(287,'Minahasa Tenggara'),
+(288,'Minahasa Utara'),
+(289,'Mojokerto'),
+(290,'Mojokerto'),
+(291,'Morowali'),
+(292,'Muara Enim'),
+(293,'Muaro Jambi'),
+(294,'Muko Muko'),
+(295,'Muna'),
+(296,'Murung Raya'),
+(297,'Musi Banyuasin'),
+(298,'Musi Rawas'),
+(299,'Nabire'),
+(300,'Nagan Raya'),
+(301,'Nagekeo'),
+(302,'Natuna'),
+(303,'Nduga'),
+(304,'Ngada'),
+(305,'Nganjuk'),
+(306,'Ngawi'),
+(307,'Nias'),
+(308,'Nias Barat'),
+(309,'Nias Selatan'),
+(310,'Nias Utara'),
+(311,'Nunukan'),
+(312,'Ogan Ilir'),
+(313,'Ogan Komering Ilir'),
+(314,'Ogan Komering Ulu'),
+(315,'Ogan Komering Ulu Selatan'),
+(316,'Ogan Komering Ulu Timur'),
+(317,'Pacitan'),
+(318,'Padang'),
+(319,'Padang Lawas'),
+(320,'Padang Lawas Utara'),
+(321,'Padang Panjang'),
+(322,'Padang Pariaman'),
+(323,'Padang Sidempuan'),
+(324,'Pagar Alam'),
+(325,'Pakpak Bharat'),
+(326,'Palangka Raya'),
+(327,'Palembang'),
+(328,'Palopo'),
+(329,'Palu'),
+(330,'Pamekasan'),
+(331,'Pandeglang'),
+(332,'Pangandaran'),
+(333,'Pangkajene Kepulauan'),
+(334,'Pangkal Pinang'),
+(335,'Paniai'),
+(336,'Parepare'),
+(337,'Pariaman'),
+(338,'Parigi Moutong'),
+(339,'Pasaman'),
+(340,'Pasaman Barat'),
+(341,'Paser'),
+(342,'Pasuruan'),
+(343,'Pasuruan'),
+(344,'Pati'),
+(345,'Payakumbuh'),
+(346,'Pegunungan Arfak'),
+(347,'Pegunungan Bintang'),
+(348,'Pekalongan'),
+(349,'Pekalongan'),
+(350,'Pekanbaru'),
+(351,'Pelalawan'),
+(352,'Pemalang'),
+(353,'Pematang Siantar'),
+(354,'Penajam Paser Utara'),
+(355,'Pesawaran'),
+(356,'Pesisir Barat'),
+(357,'Pesisir Selatan'),
+(358,'Pidie'),
+(359,'Pidie Jaya'),
+(360,'Pinrang'),
+(361,'Pohuwato'),
+(362,'Polewali Mandar'),
+(363,'Ponorogo'),
+(364,'Pontianak'),
+(365,'Pontianak'),
+(366,'Poso'),
+(367,'Prabumulih'),
+(368,'Pringsewu'),
+(369,'Probolinggo'),
+(370,'Probolinggo'),
+(371,'Pulang Pisau'),
+(372,'Pulau Morotai'),
+(373,'Puncak'),
+(374,'Puncak Jaya'),
+(375,'Purbalingga'),
+(376,'Purwakarta'),
+(377,'Purworejo'),
+(378,'Raja Ampat'),
+(379,'Rejang Lebong'),
+(380,'Rembang'),
+(381,'Rokan Hilir'),
+(382,'Rokan Hulu'),
+(383,'Rote Ndao'),
+(384,'Sabang'),
+(385,'Sabu Raijua'),
+(386,'Salatiga'),
+(387,'Samarinda'),
+(388,'Sambas'),
+(389,'Samosir'),
+(390,'Sampang'),
+(391,'Sanggau'),
+(392,'Sarmi'),
+(393,'Sarolangun'),
+(394,'Sawah Lunto'),
+(395,'Sekadau'),
+(396,'Selayar (Kepulauan Selayar)'),
+(397,'Seluma'),
+(398,'Semarang'),
+(399,'Semarang'),
+(400,'Seram Bagian Barat'),
+(401,'Seram Bagian Timur'),
+(402,'Serang'),
+(403,'Serang'),
+(404,'Serdang Bedagai'),
+(405,'Seruyan'),
+(406,'Siak'),
+(407,'Sibolga'),
+(408,'Sidenreng Rappang/Rapang'),
+(409,'Sidoarjo'),
+(410,'Sigi'),
+(411,'Sijunjung (Sawah Lunto Sijunjung)'),
+(412,'Sikka'),
+(413,'Simalungun'),
+(414,'Simeulue'),
+(415,'Singkawang'),
+(416,'Sinjai'),
+(417,'Sintang'),
+(418,'Situbondo'),
+(419,'Sleman'),
+(420,'Solok'),
+(421,'Solok'),
+(422,'Solok Selatan'),
+(423,'Soppeng'),
+(424,'Sorong'),
+(425,'Sorong'),
+(426,'Sorong Selatan'),
+(427,'Sragen'),
+(428,'Subang'),
+(429,'Subulussalam'),
+(430,'Sukabumi'),
+(431,'Sukabumi'),
+(432,'Sukamara'),
+(433,'Sukoharjo'),
+(434,'Sumba Barat'),
+(435,'Sumba Barat Daya'),
+(436,'Sumba Tengah'),
+(437,'Sumba Timur'),
+(438,'Sumbawa'),
+(439,'Sumbawa Barat'),
+(440,'Sumedang'),
+(441,'Sumenep'),
+(442,'Sungaipenuh'),
+(443,'Supiori'),
+(444,'Surabaya'),
+(445,'Surakarta (Solo)'),
+(446,'Tabalong'),
+(447,'Tabanan'),
+(448,'Takalar'),
+(449,'Tambrauw'),
+(450,'Tana Tidung'),
+(451,'Tana Toraja'),
+(452,'Tanah Bumbu'),
+(453,'Tanah Datar'),
+(454,'Tanah Laut'),
+(455,'Tangerang'),
+(456,'Tangerang'),
+(457,'Tangerang Selatan'),
+(458,'Tanggamus'),
+(459,'Tanjung Balai'),
+(460,'Tanjung Jabung Barat'),
+(461,'Tanjung Jabung Timur'),
+(462,'Tanjung Pinang'),
+(463,'Tapanuli Selatan'),
+(464,'Tapanuli Tengah'),
+(465,'Tapanuli Utara'),
+(466,'Tapin'),
+(467,'Tarakan'),
+(468,'Tasikmalaya'),
+(469,'Tasikmalaya'),
+(470,'Tebing Tinggi'),
+(471,'Tebo'),
+(472,'Tegal'),
+(473,'Tegal'),
+(474,'Teluk Bintuni'),
+(475,'Teluk Wondama'),
+(476,'Temanggung'),
+(477,'Ternate'),
+(478,'Tidore Kepulauan'),
+(479,'Timor Tengah Selatan'),
+(480,'Timor Tengah Utara'),
+(481,'Toba Samosir'),
+(482,'Tojo Una-Una'),
+(483,'Toli-Toli'),
+(484,'Tolikara'),
+(485,'Tomohon'),
+(486,'Toraja Utara'),
+(487,'Trenggalek'),
+(488,'Tual'),
+(489,'Tuban'),
+(490,'Tulang Bawang'),
+(491,'Tulang Bawang Barat'),
+(492,'Tulungagung'),
+(493,'Wajo'),
+(494,'Wakatobi'),
+(495,'Waropen'),
+(496,'Way Kanan'),
+(497,'Wonogiri'),
+(498,'Wonosobo'),
+(499,'Yahukimo'),
+(500,'Yalimo'),
+(501,'Yogyakarta');
+
+/*Table structure for table `ktp` */
+
+DROP TABLE IF EXISTS `ktp`;
 
 CREATE TABLE `ktp` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_user` int(11) NOT NULL,
   `foto_ktp` varchar(255) NOT NULL,
-  `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `status` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `ktp_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+/*Data for the table `ktp` */
 
---
--- Table structure for table `rating`
---
+/*Table structure for table `rating` */
+
+DROP TABLE IF EXISTS `rating`;
 
 CREATE TABLE `rating` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_sender` int(11) NOT NULL,
   `id_traveller` int(11) NOT NULL,
-  `rate` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `rate` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_sender` (`id_sender`),
+  KEY `id_traveller` (`id_traveller`),
+  CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`id_sender`) REFERENCES `user` (`id`),
+  CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`id_traveller`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+/*Data for the table `rating` */
 
---
--- Table structure for table `user`
---
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -94,111 +606,16 @@ CREATE TABLE `user` (
   `api_key` varchar(255) NOT NULL,
   `api_hit` int(11) NOT NULL,
   `role` varchar(255) NOT NULL,
-  `saldo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `saldo` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
+/*Data for the table `user` */
 
---
--- Indexes for table `barang`
---
-ALTER TABLE `barang`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_sender` (`id_sender`),
-  ADD KEY `id_traveller` (`id_traveller`),
-  ADD KEY `id_kota_keberangkatan` (`id_kota_keberangkatan`),
-  ADD KEY `id_kota_tujuan` (`id_kota_tujuan`);
+insert  into `user`(`id`,`username`,`password`,`email`,`no_hp`,`api_key`,`api_hit`,`role`,`saldo`) values 
+(1,'test1','12345678','test1@gmail.com','0123456789','gSkyFoaX2X',0,'Sender',0);
 
---
--- Indexes for table `kota`
---
-ALTER TABLE `kota`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `ktp`
---
-ALTER TABLE `ktp`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user` (`id_user`);
-
---
--- Indexes for table `rating`
---
-ALTER TABLE `rating`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_sender` (`id_sender`),
-  ADD KEY `id_traveller` (`id_traveller`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `barang`
---
-ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `kota`
---
-ALTER TABLE `kota`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `ktp`
---
-ALTER TABLE `ktp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `rating`
---
-ALTER TABLE `rating`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `barang`
---
-ALTER TABLE `barang`
-  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_sender`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `barang_ibfk_2` FOREIGN KEY (`id_traveller`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `barang_ibfk_3` FOREIGN KEY (`id_kota_keberangkatan`) REFERENCES `kota` (`id`),
-  ADD CONSTRAINT `barang_ibfk_4` FOREIGN KEY (`id_kota_tujuan`) REFERENCES `kota` (`id`);
-
---
--- Constraints for table `ktp`
---
-ALTER TABLE `ktp`
-  ADD CONSTRAINT `ktp_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
-
---
--- Constraints for table `rating`
---
-ALTER TABLE `rating`
-  ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`id_sender`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`id_traveller`) REFERENCES `user` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- Project/src/controllers/temp.js
+++ Project/src/controllers/temp.js
@@ -3,13 +3,34 @@ const express = require("express");
 const Joi = require("joi");
 
 const Temp = require("../models/temp");
+const Kota = require("../models/Kota");
 
 const tempQuery = async (req, res) => {
-    const result = await axios.get("https://api.rajaongkir.com/starter/city");
-
-    return res.status(201).json({
-        msg: "Ha",
-    });
+    try {
+        const result = await axios.get(
+            "https://api.rajaongkir.com/starter/city",
+            {
+                headers: {
+                    key: "59162edb62cc968ea7148fc4833d2316",
+                },
+            }
+        );
+        for (let i = 0; i < result.data.rajaongkir.results.length; i++) {
+            const element = result.data.rajaongkir.results[i];
+            await Kota.create({
+                id: element.city_id,
+                nama: element.city_name,
+            });
+        }
+        return res.status(201).json({
+            msg: result.data.rajaongkir.results,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(201).json({
+            msg: "Ha",
+        });
+    }
 };
 
 module.exports = {
