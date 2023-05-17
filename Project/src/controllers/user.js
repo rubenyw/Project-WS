042c0abd64927f24ad90d6aeed3f99db10e4d75b
rubenyw <rubenyasonwinarta@gmail.com>
2023-05-17 22:43:43 +0700

    Revert "registerTraveller, loginTraveller (complete)"
    This reverts commit d789fd24ab0063e10feb15f068bbb9ba60de18d4.

--- Project/src/controllers/user.js
+++ Project/src/controllers/user.js
@@ -1,5 +1,4 @@
 const User = require("../models/User");
-const Rating = require("../models/Rating");
 const Joi = require("joi");
 
 const loginSchema = Joi.object({
@@ -73,24 +72,15 @@ const loginSender = async (req, res) => {
         const { username, password } = value;
 
         // Find the user in the database
-        const user = await User.findOne({
-            where: {
-                username: username,
-            },
-        });
+        const user = await User.findOne({ username });
 
         if (!user) {
             res.status(404).json({ error: "User not found." });
             return;
         }
 
-        if (password != user.password) {
+        if (password == user.password) {
             res.status(401).json({ error: "Invalid password." });
-            return;
-        }
-
-        if (user.role != "Sender") {
-            res.status(401).json({ error: "Bukan Sender." });
             return;
         }
 
@@ -100,88 +90,8 @@ const loginSender = async (req, res) => {
         res.status(500).json({ error: "An error occurred while processing the login." });
     }
 };
-const registerTraveller = async (req, res) => {
-    try {
-        const { error, value } = registerSchema.validate(req.body);
-
-        if (error) {
-            res.status(400).json({ error: error.details[0].message });
-            return;
-        }
-
-        const { username, password, email, nomor_hp } = value;
-
-        // Generate the API key
-        const api_key = generateApiKey(10);
-
-        // Other Variables
-        const api_hit = 0;
-        const saldo = 0;
-        const role = "Traveller";
-
-        // Create a new user in the database
-        const newUser = await User.create({
-            username,
-            password,
-            email,
-            no_hp: nomor_hp,
-            api_key,
-            api_hit,
-            saldo,
-            role,
-        });
-
-        res.status(201).json({ api_key });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "An error occurred while creating the user." });
-    }
-};
-const loginTraveller = async (req, res) => {
-    try {
-        const { error, value } = loginSchema.validate(req.body);
-
-        if (error) {
-            res.status(400).json({ error: error.details[0].message });
-            return;
-        }
-
-        const { username, password } = value;
-
-        // Find the user in the database
-        const user = await User.findOne({
-            where: {
-                username: username,
-            },
-        });
-
-        if (!user) {
-            res.status(404).json({ error: "User not found." });
-            return;
-        }
-
-        if (password != user.password) {
-            res.status(401).json({ error: "Invalid password." });
-            return;
-        }
-        console.log(user);
-
-        if (user.role !== "Traveller") {
-            res.status(401).json({ error: "Bukan Traveller." });
-            return;
-        }
-
-        // Calculate the average rating for the user
-        const ratings = await Rating.findAll({ where: { id_traveller: user.id } });
-        const ratingsSum = ratings.reduce((acc, rating) => acc + rating.rate, 0);
-        const averageRating = ratings.length ? ratingsSum / ratings.length : 0;
-
-        res.status(200).json({ api_key: user.api_key, average_rating: averageRating });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "An error occurred while processing the login." });
-    }
-};
+const registerTraveller = (req, res) => {};
+const loginTraveller = (req, res) => {};
 
 module.exports = {
     registerSender,
