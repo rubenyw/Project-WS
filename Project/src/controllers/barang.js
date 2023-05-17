23b6d1b25bb68ad6c2c1cc0b4fc02390dad6351f
rubenyw <rubenyasonwinarta@gmail.com>
2023-05-17 23:18:55 +0700

    update

--- Project/src/controllers/barang.js
+++ Project/src/controllers/barang.js
@@ -22,6 +22,34 @@ const requestKirimBarang = async (req, res) => {
     }
 };
 
+const requestEditBarang = async (req, res) => {
+    let id_kirimBarang = req.query.id_kirimBarang;
+    const barang = await Barang.findByPk(id_kirimBarang);
+    if (!barang) {
+        return res.status(404).json({
+            status: 404,
+            msg: "Kode Barang tidak valid!",
+        });
+    }
+
+    const schema = Joi.object({
+        nama_barang: Joi.string().required(),
+        berat_barang: Joi.number().required(),
+        asal_barang: Joi.string().required(),
+        tujuan_barang: Joi.string().required(),
+    });
+
+    try {
+        await schema.validateAsync(req.body);
+    } catch (error) {
+        return res.status(404).json({
+            status: 404,
+            msg: error.toString(),
+        });
+    }
+};
+
 module.exports = {
     requestKirimBarang,
+    requestEditBarang,
 };
