f29f1dd6c5ce7283c15b61017fb1d4d9fb517af8
rubenyw <rubenyasonwinarta@gmail.com>
2023-05-18 00:11:21 +0700

    Update index.js
    hapus send barang

--- Project/src/routes/index.js
+++ Project/src/routes/index.js
@@ -14,7 +14,6 @@ router.post("/sender/register", registerSender);
 router.post("/traveller/register", registerTraveller);
 router.post("/sender/login", loginSender);
 router.post("/traveller/login", loginTraveller);
-router.post("/sender/request", sendBarang);
 
 // Variabel function untuk barang
 const {
