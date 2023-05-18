4856d232f8c2a3a7890f01f12c61943b9c3dbc5f
rubenyw <rubenyasonwinarta@gmail.com>
2023-05-18 15:35:57 +0700

    Create request.js

---
+++ Project/src/controllers/request.js
@@ -0,0 +1,10 @@
+const { default: axios } = require("axios");
+const express = require("express");
+const Joi = require("joi");
+
+const topUpRequest = async (req, res) => {};
+const setRequestPerjalanan = async (req, res) => {};
+const lihatRequestPerjalanan = async (req, res) => {};
+const rating = async (req, res) => {};
+
+module.exports = { topUpRequest, rating };
