5985cf2e6ba15d80cddc92801c6bcdd618fe8a23
rubenyw <rubenyasonwinarta@gmail.com>
2023-05-20 14:35:49 +0700

    Update saldo.js

--- Project/src/validation/saldo.js
+++ Project/src/validation/saldo.js
@@ -2,21 +2,21 @@ const Joi = require("joi");
 
 const kuota = Joi.object({
     password: Joi.string().required().label("User Password").messages({
-        "any.required": "password perlu diisi",
+        "any.required": "{{#label}} perlu diisi",
     }),
     jumlah: Joi.number().required().min(1).messages({
-        "any.required": "nominal perlu diisi",
-        "number.min": "nominal pembelian minimal 1",
+        "any.required": "{{#label}} perlu diisi",
+        "number.min": "{{#label}} pembelian minimal 1",
     }),
 });
 
 const saldo = Joi.object({
     password: Joi.string().required().label("User Password").messages({
-        "any.required": "password perlu diisi",
+        "any.required": "{{label}} perlu diisi",
     }),
     jumlah: Joi.number().required().min(1000).messages({
-        "any.required": "nominal perlu diisi",
-        "number.min": "nominal pembelian minimal 1000",
+        "any.required": "{{label}} perlu diisi",
+        "number.min": "{{label}} pembelian minimal 1000",
     }),
 });
 
