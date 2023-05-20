10b0add34e70c069f3976e237c70d9badf617709
rubenyw <rubenyasonwinarta@gmail.com>
2023-05-20 15:24:52 +0700

    Update perjalanan.js

--- Project/src/validation/perjalanan.js
+++ Project/src/validation/perjalanan.js
@@ -1,1 +1,15 @@
 const Joi = require("joi");
+
+const Kota = require("../models/Kota");
+
+const cekKotaBerangkat = async (kota_keberangkatan) => {};
+
+const set = Joi.object({
+    kota_keberangkatan: Joi.string()
+        .required()
+        .external()
+        .label("Kota Keberangkatan")
+        .messages({
+            "any.required": "{{#label}} perlu diisi",
+        }),
+});
