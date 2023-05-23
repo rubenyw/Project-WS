eae3b3e21faa6c8479b040ba916c2a443edce330
rubenyw <rubenyasonwinarta@gmail.com>
2023-05-23 11:32:52 +0700

    Update barang.js

--- Project/src/validation/barang.js
+++ Project/src/validation/barang.js
@@ -1,50 +1,51 @@
 const Joi = require("joi");
 
 const kirim = Joi.object({
-    nama_barang: Joi.string()
-        .required()
-        .label("Nama Barang")
-        .messages({ "any.required": "{{#label}} Perlu di Isi" }),
-    berat_barang: Joi.number()
-        .required()
-        .label("Berat Barang")
-        .messages({ "any.required": "{{#label}} Perlu di Isi" }),
-    asal_barang: Joi.string()
-        .required()
-        .label("Asal Barang")
-        .messages({ "any.required": "{{#label}} Perlu di Isi" }),
-    tujuan_barang: Joi.string()
-        .required()
-        .label("Tujuan Barang")
-        .messages({ "any.required": "{{#label}} Perlu di Isi" }),
+    nama_barang: Joi.string().required().label("Nama Barang").messages({
+        "any.required": "{{#label}} Perlu di Isi",
+        "number.empty": "{{#label}} perlu diisi, tidak boleh kosong",
+    }),
+    berat_barang: Joi.number().required().label("Berat Barang").messages({
+        "any.required": "{{#label}} Perlu di Isi",
+        "number.empty": "{{#label}} perlu diisi, tidak boleh kosong",
+    }),
+    asal_barang: Joi.string().required().label("Asal Barang").messages({
+        "any.required": "{{#label}} Perlu di Isi",
+        "string.empty": "{{#label}} perlu diisi, tidak boleh kosong",
+    }),
+    tujuan_barang: Joi.string().required().label("Tujuan Barang").messages({
+        "any.required": "{{#label}} Perlu di Isi",
+        "string.empty": "{{#label}} perlu diisi, tidak boleh kosong",
+    }),
 });
 
 const edit = Joi.object({
     id_barang: Joi.string().required().label("ID Barang").messages({
         "any.required": "{{#label}} Perlu di Isi",
-        "string.external": "{{#label}} tidak valid",
+        "string.empty": "{{#label}} perlu diisi, tidak boleh kosong",
     }),
-    nama_barang: Joi.string()
-        .required()
-        .label("Nama Barang")
-        .messages({ "any.required": "{{#label}} Perlu di Isi" }),
-    berat_barang: Joi.number()
-        .required()
-        .label("Berat Barang")
-        .messages({ "any.required": "{{#label}} Perlu di Isi" }),
-    asal_barang: Joi.string()
-        .required()
-        .label("Asal Barang")
-        .messages({ "any.required": "{{#label}} Perlu di Isi" }),
-    tujuan_barang: Joi.string()
-        .required()
-        .label("Tujuan Barang")
-        .messages({ "any.required": "{{#label}} Perlu di Isi" }),
+    nama_barang: Joi.string().required().label("Nama Barang").messages({
+        "any.required": "{{#label}} Perlu di Isi",
+        "string.empty": "{{#label}} perlu diisi, tidak boleh kosong",
+    }),
+    berat_barang: Joi.number().required().label("Berat Barang").messages({
+        "any.required": "{{#label}} Perlu di Isi",
+        "number.empty": "{{#label}} perlu diisi, tidak boleh kosong",
+    }),
+    asal_barang: Joi.string().required().label("Asal Barang").messages({
+        "any.required": "{{#label}} Perlu di Isi",
+        "string.empty": "{{#label}} perlu diisi, tidak boleh kosong",
+    }),
+    tujuan_barang: Joi.string().required().label("Tujuan Barang").messages({
+        "any.required": "{{#label}} Perlu di Isi",
+        "string.empty": "{{#label}} perlu diisi, tidak boleh kosong",
+    }),
 });
 
 const terima = Joi.object({
     id_barang: Joi.number().required().label("ID Barang").messages({
         "any.required": "Mohon {{#label}} Barang untuk diisi terlebih dahulu",
+        "number.empty": "{{#label}} perlu diisi, tidak boleh kosong",
         "number.base": "{{#label}} harus diisi dengan benar",
     }),
 });
@@ -52,6 +53,7 @@ const terima = Joi.object({
 const rate = Joi.object({
     id_barang: Joi.number().required().label("ID Barang").messages({
         "any.required": "Mohon {{#label}} Barang untuk diisi terlebih dahulu",
+        "number.empty": "{{#label}} perlu diisi, tidak boleh kosong",
         "number.base": "{{#label}} harus diisi dengan benar",
     }),
     rating: Joi.number()
@@ -63,6 +65,7 @@ const rate = Joi.object({
             "any.required":
                 "Mohon {{#label}} Barang untuk diisi terlebih dahulu",
             "number.base": "{{#label}} harus diisi dengan benar",
+            "number.empty": "{{#label}} perlu diisi, tidak boleh kosong",
             "number.min": "{{#label}} rating paling kecil hanya bisa diisi 1",
             "number.max": "{{#label}} rating paling besar hanya bisa diisi 10",
         }),
