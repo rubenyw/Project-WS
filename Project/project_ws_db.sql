fe7709390b333c7cfbe400682cd2f608913dd4d3
rubenyw <rubenyasonwinarta@gmail.com>
2023-05-28 16:29:04 +0700

    Update project_ws_db.sql

--- Project/project_ws_db.sql
+++ Project/project_ws_db.sql
@@ -1,6 +1,11 @@
 /*
+<<<<<<< HEAD
 SQLyog Community v13.1.9 (64 bit)
 MySQL - 10.4.24-MariaDB : Database - project_ws
+=======
+SQLyog Community v13.2.0 (64 bit)
+MySQL - 10.4.27-MariaDB : Database - project_ws
+>>>>>>> parent of 09a9199 (update database)
 *********************************************************************
 */
 
@@ -12,6 +17,7 @@ MySQL - 10.4.24-MariaDB : Database - project_ws
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+<<<<<<< HEAD
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`project_ws` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 USE `project_ws`;
@@ -239,6 +245,14 @@ insert  into `aviation`(`id`,`nama`,`iata_code`) values
 
 /*Table structure for table `barang` */
 
+=======
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`project_ws` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `project_ws`;
+
+/*Table structure for table `barang` */
+
+>>>>>>> parent of 09a9199 (update database)
 DROP TABLE IF EXISTS `barang`;
 
 CREATE TABLE `barang` (
@@ -261,7 +275,11 @@ CREATE TABLE `barang` (
   CONSTRAINT `barang_ibfk_2` FOREIGN KEY (`id_traveller`) REFERENCES `user` (`id`),
   CONSTRAINT `barang_ibfk_3` FOREIGN KEY (`id_kota_keberangkatan`) REFERENCES `kota` (`id`),
   CONSTRAINT `barang_ibfk_4` FOREIGN KEY (`id_kota_tujuan`) REFERENCES `kota` (`id`)
+<<<<<<< HEAD
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+=======
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+>>>>>>> parent of 09a9199 (update database)
 
 /*Data for the table `barang` */
 
@@ -272,6 +290,7 @@ DROP TABLE IF EXISTS `kota`;
 CREATE TABLE `kota` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(255) NOT NULL,
+<<<<<<< HEAD
   `id_rajaongkir` int(11) DEFAULT NULL,
   `id_flightapi` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -318,6 +337,112 @@ insert  into `kota`(`id`,`nama`,`id_rajaongkir`,`id_flightapi`) values
 (91,'TASIKMALAYA',469,7565),
 (94,'TERNATE',477,7571),
 (98,'YOGYAKARTA',501,3406);
+=======
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=600 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `kota` */
+
+insert  into `kota`(`id`,`nama`) values 
+(14,'AMBON'),
+(19,'BALIKPAPAN'),
+(20,'BANDA ACEH'),
+(21,'BANDAR LAMPUNG'),
+(23,'BANDUNG'),
+(34,'BANJAR'),
+(35,'BANJARBARU'),
+(36,'BANJARMASIN'),
+(48,'BATAM'),
+(51,'BATU'),
+(53,'BAU-BAU'),
+(55,'BEKASI'),
+(62,'BENGKULU'),
+(69,'BIMA'),
+(70,'BINJAI'),
+(73,'BITUNG'),
+(75,'BLITAR'),
+(79,'BOGOR'),
+(89,'BONTANG'),
+(93,'BUKITTINGGI'),
+(106,'CILEGON'),
+(107,'CIMAHI'),
+(109,'CIREBON'),
+(114,'DENPASAR'),
+(115,'DEPOK'),
+(120,'DUMAI'),
+(130,'GORONTALO'),
+(137,'GUNUNGSITOLI'),
+(151,'JAKARTA BARAT'),
+(152,'JAKARTA PUSAT'),
+(153,'JAKARTA SELATAN'),
+(154,'JAKARTA TIMUR'),
+(155,'JAKARTA UTARA'),
+(156,'JAMBI'),
+(158,'JAYAPURA'),
+(179,'KEDIRI'),
+(182,'KENDARI'),
+(204,'KOTAMOBAGU'),
+(213,'KUPANG'),
+(230,'LANGSA'),
+(235,'LHOKSEUMAWE'),
+(242,'LUBUK LINGGAU'),
+(248,'MADIUN'),
+(250,'MAGELANG'),
+(254,'MAKASSAR'),
+(256,'MALANG'),
+(267,'MANADO'),
+(276,'MATARAM'),
+(278,'MEDAN'),
+(283,'METRO'),
+(290,'MOJOKERTO'),
+(318,'PADANG'),
+(321,'PADANG PANJANG'),
+(323,'PADANG SIDEMPUAN'),
+(324,'PAGAR ALAM'),
+(326,'PALANGKA RAYA'),
+(327,'PALEMBANG'),
+(328,'PALOPO'),
+(329,'PALU'),
+(334,'PANGKAL PINANG'),
+(336,'PAREPARE'),
+(337,'PARIAMAN'),
+(343,'PASURUAN'),
+(345,'PAYAKUMBUH'),
+(349,'PEKALONGAN'),
+(350,'PEKANBARU'),
+(353,'PEMATANG SIANTAR'),
+(365,'PONTIANAK'),
+(367,'PRABUMULIH'),
+(370,'PROBOLINGGO'),
+(384,'SABANG'),
+(386,'SALATIGA'),
+(387,'SAMARINDA'),
+(394,'SAWAH LUNTO'),
+(399,'SEMARANG'),
+(403,'SERANG'),
+(407,'SIBOLGA'),
+(415,'SINGKAWANG'),
+(421,'SOLOK'),
+(425,'SORONG'),
+(429,'SUBULUSSALAM'),
+(431,'SUKABUMI'),
+(442,'SUNGAIPENUH'),
+(444,'SURABAYA'),
+(445,'SURAKARTA (SOLO)'),
+(456,'TANGERANG'),
+(457,'TANGERANG SELATAN'),
+(459,'TANJUNG BALAI'),
+(462,'TANJUNG PINANG'),
+(467,'TARAKAN'),
+(469,'TASIKMALAYA'),
+(470,'TEBING TINGGI'),
+(473,'TEGAL'),
+(477,'TERNATE'),
+(478,'TIDORE KEPULAUAN'),
+(485,'TOMOHON'),
+(488,'TUAL'),
+(501,'YOGYAKARTA');
+>>>>>>> parent of 09a9199 (update database)
 
 /*Table structure for table `ktp` */
 
@@ -330,16 +455,27 @@ CREATE TABLE `ktp` (
   PRIMARY KEY (`id_user`),
   KEY `id_user` (`id_user`),
   CONSTRAINT `ktp_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`)
+<<<<<<< HEAD
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `ktp` */
 
 /*Table structure for table `rajaongkir` */
+=======
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `ktp` */
+
+/*Table structure for table `perjalanan` */
+
+DROP TABLE IF EXISTS `perjalanan`;
+>>>>>>> parent of 09a9199 (update database)
 
 DROP TABLE IF EXISTS `rajaongkir`;
 
 CREATE TABLE `rajaongkir` (
   `id` int(11) NOT NULL,
+<<<<<<< HEAD
   `nama` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -446,6 +582,13 @@ insert  into `rajaongkir`(`id`,`nama`) values
 (488,'TUAL'),
 (501,'YOGYAKARTA');
 
+=======
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `perjalanan` */
+
+>>>>>>> parent of 09a9199 (update database)
 /*Table structure for table `rating` */
 
 DROP TABLE IF EXISTS `rating`;
@@ -460,7 +603,11 @@ CREATE TABLE `rating` (
   KEY `id_traveller` (`id_traveller`),
   CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`id_sender`) REFERENCES `user` (`id`),
   CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`id_traveller`) REFERENCES `user` (`id`)
+<<<<<<< HEAD
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+=======
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+>>>>>>> parent of 09a9199 (update database)
 
 /*Data for the table `rating` */
 
@@ -479,7 +626,11 @@ CREATE TABLE `user` (
   `role` varchar(255) NOT NULL,
   `saldo` int(11) NOT NULL,
   PRIMARY KEY (`id`)
+<<<<<<< HEAD
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+=======
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+>>>>>>> parent of 09a9199 (update database)
 
 /*Data for the table `user` */
 
