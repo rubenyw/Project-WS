45f60f36942498760b9b262cf9c070e067b0c5d7
rubenyw <rubenyasonwinarta@gmail.com>
2023-06-21 01:16:56 +0700

    Update database.js

--- Project/src/databases/database.js
+++ Project/src/databases/database.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize('project_ws', 'root', '', {
-    host: 'localhost',
+const db = new Sequelize("project_ws", "root", "", {
+    host: "localhost",
     port: 3306,
-    dialect: "mysql"
-})
+    dialect: "mysql",
+});
 
 module.exports = db;
