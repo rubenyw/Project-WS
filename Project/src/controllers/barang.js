d70eb4f9854a5da28e9ebcef97117322265c969a
rubenyw <rubenyasonwinarta@gmail.com>
2023-06-20 20:25:04 +0700

    update postman

--- Project/src/controllers/barang.js
+++ Project/src/controllers/barang.js
@@ -326,6 +326,12 @@ const complete_request = async (req, res) => {
             id: barang,
         },
     });
+    if (!databarang) {
+        return res.status(404).json({
+            status: 404,
+            msg: "Barang not found",
+        });
+    }
     databarang.update({
         status: "DONE",
     });
