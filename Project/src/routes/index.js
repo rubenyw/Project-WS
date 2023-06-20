2638bc394c03f0cfde0cda79fb88cca95562bb14
rubenyw <rubenyasonwinarta@gmail.com>
2023-06-21 01:18:01 +0700

    Update index.js

--- Project/src/routes/index.js
+++ Project/src/routes/index.js
@@ -2,13 +2,13 @@
 const express = require("express");
 const router = express.Router();
 
-// Router untuk debuggin sementara
+// DEBUGGING
 const { insertRajaOngkir, insertAviation, insertKota } = require("../controllers/temp_createdatabase");
+
+// Router untuk debuggin sementara
 router.get("/raja_ongkir", insertRajaOngkir);
 router.get("/aviation", insertAviation);
 router.get("/kota", insertKota);
-// router.get("/create", aviationQuery);
-// router.get("/update", update);
 
 // testing API, nanti Delete
 const { testing, city, country } = require("../controllers/temp_testapi.js");
