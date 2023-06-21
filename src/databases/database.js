fe16504112d2d5f0a17dbb6ec40825ace6d7d403
rubenyw <rubenyasonwinarta@gmail.com>
2023-06-22 00:22:56 +0700

    test

--- src/databases/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const db = new Sequelize("project_ws", "root", "", {
-    host: "localhost",
-    port: 3306,
-    dialect: "mysql",
-});
-
-module.exports = db;
